b595c196c22bffc49b1172ba6e2f2c3826b910cb
Alan Field <afield@redhat.com>
2020-02-02 17:18:19 +0000

    :open_file_folder: Update openshift specific files.

---
+++ openshift/e2e-tests-openshift.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+
+source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/e2e-tests.sh
+
+set -x
+
+function run_e2e_tests(){
+  header "Running tests"
+  echo "The E2E test suite is currently empty"
+}
+
+failed=0
+
+run_e2e_tests || failed=1
+
+(( failed )) && exit 1
+
+success
