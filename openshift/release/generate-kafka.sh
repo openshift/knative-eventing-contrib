adc27026a02438c70a07f0581e83a1101db22279
Matthias Wessendorf <mwessend@redhat.com>
2020-09-07 11:17:40 +0200

    Updating kafka release script
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/release/generate-kafka.sh
+++ openshift/release/generate-kafka.sh
@@ -7,7 +7,7 @@ release=$1
 output_file="openshift/release/knative-eventing-kafka-contrib-${release}.yaml"
 
 if [ $release = "ci" ]; then
-    image_prefix="image-registry.openshift-image-registry.svc:5000/knative-eventing/knative-eventing-contrib-"
+    image_prefix="registry.svc.ci.openshift.org/openshift/knative-nightly:knative-eventing-sources-"
     tag=""
 else
     image_prefix="quay.io/openshift-knative/knative-eventing-sources-"
@@ -15,11 +15,11 @@ else
 fi
 
 # Apache Kafka Source
-resolve_resources kafka/source/config/ kafka-resolved.yaml $image_prefix $release
+resolve_resources kafka/source/config/ kafka-resolved.yaml $image_prefix $tag
 cat kafka-resolved.yaml > $output_file
 rm kafka-resolved.yaml
 
 # Apache Kafka Source
-resolve_resources kafka/channel/config/ kafka-resolved.yaml $image_prefix $release
+resolve_resources kafka/channel/config/ kafka-resolved.yaml $image_prefix $tag
 cat kafka-resolved.yaml >> $output_file
 rm kafka-resolved.yaml
